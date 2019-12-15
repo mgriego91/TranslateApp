70879a836813be3dbc9ef1eda9d50769f3d7114e
mgriego91 <57187570+mgriego91@users.noreply.github.com>
2019-12-15 16:30:45 -0500

    Update MainActivity.java

--- src/main/java/com/example/testagain/MainActivity.java
+++ src/main/java/com/example/testagain/MainActivity.java
@@ -12,20 +12,35 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import cz.msebera.android.httpclient.Header;
 
+/**
+ * This is the Main driver for the WebsScrape and Http classes
+ */
 public class MainActivity extends AppCompatActivity {
-    private TextView translationTextView;
+    private TextView translationTextView;// Creates object to allow for view of text after translation
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // Runs code after translation and buttons are clicked in App
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.translateButton);
-        final EditText input = findViewById(R.id.textInput);
-        translationTextView = findViewById(R.id.translation);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main); // Utilizes code from activity_main.xml file to show view
+        Button button = findViewById(R.id.translateButton); // Initializes button to run code
+        final EditText input = findViewById(R.id.textInput); // For user input text
+        translationTextView = findViewById(R.id.translation); // Puts value in the TextView
+        button.setOnClickListener(new View.OnClickListener() { // Runs to "listen" for the button click
+            /**
+             * This method is Android standard to run code as soon as a button in the app is clicked
+             * @param v is passed in
+             */
             @Override
             public void onClick(View v) {
-                final String translationString = input.getText().toString();
+                final String translationString = input.getText().toString(); // Places user text(word to translate) into string
+                // Uses Http class to gather information from URL and then parses
                 Http.post(translationString, "en", "ko", new JsonHttpResponseHandler(){
+                    /**
+                     * This is for successful connection to internet to ensure the URL can be reached
+                     * which is imperative for successful app usage.
+                     * @param statusCode Returns code depending on connection
+                     * @param headers Array for collecting headers
+                     * @param response Returns response of connection
+                     */
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                         try {
@@ -43,4 +58,3 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
